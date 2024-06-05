8ecf8a119d9a064347655c06932b2f09acba99b5
Rasolofondraibe <raobijaonanoah3@gmail.com>
2024-06-05 17:26:51 +0200

    ok

--- routes/utilisateur_routes.js
+++ routes/utilisateur_routes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const utilisateurController = require('../controllers/utilisateur_controller');
 
 router.post('/', utilisateurController.login);
+router.get('/localisations', utilisateurController.get_all_localisation);
+router.get('/stat_billet_etudiant', utilisateurController.stat_billet_etudiant);
 
 module.exports = router;
