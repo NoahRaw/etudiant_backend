2b48cabd5af745bff3706e9cae19351cceb8b069
Rasolofondraibe <raobijaonanoah3@gmail.com>
2024-06-12 13:51:06 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,12 +5,13 @@ const app = express();
 const port = 2000;
 const utilisateurRoutes = require('../routes/utilisateur_routes');
 const billetRoutes = require('../routes/billet_routes');
+const packRoutes = require('../routes/pack_routes');
 
 app.use(express.json());
 
 // Utiliser CORS avec des options spécifiques
 app.use(cors({
-  origin: 'https://etudiant.netlify.app', // L'origine que vous souhaitez autoriser
+  origin: 'http//localhost:3000', // L'origine que vous souhaitez autoriser
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 }));
@@ -18,6 +19,7 @@ app.use(cors({
 // Utilisation des routes
 app.use('/utilisateurs', utilisateurRoutes);
 app.use('/billet', billetRoutes);
+app.use('/pack', packRoutes);
 
 app.listen(port, () => {
   console.log(`Serveur en écoute sur http://localhost:${port}`);
