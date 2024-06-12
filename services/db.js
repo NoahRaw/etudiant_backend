e104921399419c2ef773e7c7abf6922f35264581
Rasolofondraibe <raobijaonanoah3@gmail.com>
2024-06-12 11:21:21 +0200

    ok

--- services/db.js
+++ services/db.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 // Configuration de la connexion à la base de données
 const pool = new Pool({
-  user: 'postgres',       // Remplacez par votre nom d'utilisateur PostgreSQL
-  host: 'localhost',               // Remplacez par l'hôte de votre base de données
-  database: 'etudiant', // Remplacez par le nom de votre base de données
-  password: '0000',  // Remplacez par votre mot de passe PostgreSQL
+  user: 'testdeploiment',       // Remplacez par votre nom d'utilisateur PostgreSQL
+  host: 'postgresql-testdeploiment.alwaysdata.net',               // Remplacez par l'hôte de votre base de données
+  database: 'testdeploiment_etudiant', // Remplacez par le nom de votre base de données
+  password: 'Majoreni3/',  // Remplacez par votre mot de passe PostgreSQL
   port: 5432,                      // Port PostgreSQL par défaut
 });
 
