0e91efdf3dcebee6ed49c7eea6c99897da1f3fc1
Rasolofondraibe <raobijaonanoah3@gmail.com>
2024-06-12 12:50:35 +0200

    ok

--- controllers/billet_controller.js
+++ controllers/billet_controller.js
@@ -30,17 +30,5 @@ exports.csv = async (req, res) => {
   const filePath = req.file.path;
   const results = [];
 
-  fs.createReadStream(filePath)
-    .pipe(csv())
-    .on('data', (data) => results.push(data))
-    .on('end', async () => {
-      // Suppression du fichier après traitement
-      await Billet.create_Vente_billet_temp(results);
-      fs.unlinkSync(filePath);
-      res.json(results);
-    })
-    .on('error', (error) => {
-      console.error('Error processing CSV file:', error);
-      res.status(500).json({ error: 'Error processing CSV file' });
-    });
+  res.status(201);
 };
--- routes/billet_routes.js
+++ routes/billet_routes.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const utilisateurController = require('../controllers/billet_controller');
-const multer = require('multer');
-
-// Configuration de multer pour gérer les fichiers téléchargés
-const upload = multer({ dest: 'uploads/' });
 
 router.post('/vente', utilisateurController.create_vente_billet);
 router.get('/get_all_packs', utilisateurController.get_all_packs);
-router.post('/csv', upload.single('file'), utilisateurController.csv);
+router.post('/csv', utilisateurController.csv);
 
 module.exports = router;
