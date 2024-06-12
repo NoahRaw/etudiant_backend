7cc1468225569e6422781e09d73da5e35ff415f3
Rasolofondraibe <raobijaonanoah3@gmail.com>
2024-06-12 11:11:41 +0200

    ok

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 2000;
+// const port = 2000;
 const utilisateurRoutes = require('../routes/utilisateur_routes');
 const billetRoutes = require('../routes/billet_routes');
 
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // Utiliser CORS avec des options spécifiques
 app.use(cors({
-  origin: 'http://localhost:3000', // L'origine que vous souhaitez autoriser
+  origin: 'https://etudiant.netlify.app', // L'origine que vous souhaitez autoriser
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 }));
@@ -19,8 +19,8 @@ app.use(cors({
 app.use('/utilisateurs', utilisateurRoutes);
 app.use('/billet', billetRoutes);
 
-app.listen(port, () => {
-  console.log(`Serveur en écoute sur http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Serveur en écoute sur http://localhost:${port}`);
+// });
 
 module.exports = app;
